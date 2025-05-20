c7928636208f4a1a1557451f4279dd6cf28d833c
Adham Hamdy <adhamhn333@gmail.com>
2025-05-20 21:19:26 +0300

    Setup project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ahn.boardblitz {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ahn.boardblitz to javafx.fxml;
+    exports ahn.boardblitz;
+}
